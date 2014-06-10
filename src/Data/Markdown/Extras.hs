05100745e634135797984643555c0bd7bae50e05
rejuvyesh <mail@rejuvyesh.com>
2014-06-10 19:49:46 -0400

    content left

--- src/Data/Markdown/Extras.hs
+++ src/Data/Markdown/Extras.hs
@@ -1,3 +1,28 @@
 module Data.Markdown.Extras (encodeMarkdown) where
 
-encodeMarkdown :: a -> String
+import Control.Applicative ((<$>))
+import Data.Monoid ((<>))
+import Data.Maybe (fromMaybe)
+import Text.Kindle.Clippings.Types
+import Data.List.Extras (substitute)
+
+getTitle :: Clipping -> String
+getTitle = title . document
+
+getAuthor :: Clipping -> Maybe String
+getAuthor = author . document
+
+getContent :: Clipping -> Content
+getContent = content
+
+isHighlight :: Clipping -> Bool
+isHighlight Clipping{..} = case content of
+  Highlight _ -> True
+  _           -> False
+
+encodeMarkdown :: Clipping -> String
+encodeMarkdown c = "### " ++ title' ++ "\n\n" ++ "\n" ++ author'
+  where title'   = getTitle c
+        author'  = fromMaybe "[clippings2md]" $ (" - "<>) <$> getAuthor c
+        -- content' = substitute '\n' ' ' $ show $ getContent c
+        
