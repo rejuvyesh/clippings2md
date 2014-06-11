4585e98c8b804bdb740a1505484530d430767aa1
rejuvyesh <mail@rejuvyesh.com>
2014-06-11 17:21:34 -0400

    this works for now

--- src/Data/Markdown/Extras.hs
+++ src/Data/Markdown/Extras.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards, LambdaCase #-}
 module Data.Markdown.Extras (encodeMarkdown) where
 
 import Control.Applicative ((<$>))
@@ -5,6 +7,8 @@ import Data.Monoid ((<>))
 import Data.Maybe (fromMaybe)
 import Text.Kindle.Clippings.Types
 import Data.List.Extras (substitute)
+import Text.Pandoc
+import Text.Pandoc.Builder as TPB
 
 getTitle :: Clipping -> String
 getTitle = title . document
@@ -12,17 +16,19 @@ getTitle = title . document
 getAuthor :: Clipping -> Maybe String
 getAuthor = author . document
 
-getContent :: Clipping -> Content
-getContent = content
+-- getContent :: Clipping -> String
+-- getContent = show content
 
 isHighlight :: Clipping -> Bool
 isHighlight Clipping{..} = case content of
   Highlight _ -> True
   _           -> False
 
-encodeMarkdown :: Clipping -> String
-encodeMarkdown c = "### " ++ title' ++ "\n\n" ++ "\n" ++ author'
-  where title'   = getTitle c
-        author'  = fromMaybe "[clippings2md]" $ (" - "<>) <$> getAuthor c
+encodeMarkdown :: Clipping -> Blocks
+encodeMarkdown c = (header 3 $ text (getTitle c)) <> (para $ text $ (fromMaybe "[clippings2md]" $ (" - "<>) <$> getAuthor c))
+                     -- -- Plain [Str (fromMaybe "[clippings2md]" $ (" - "<>) <$> getAuthor c)]
+                     -- Plain [Str (getContent c)]
+                   -- ]
+
         -- content' = substitute '\n' ' ' $ show $ getContent c
         
