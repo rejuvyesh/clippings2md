aa2310f8e274fb27a4edf0c5a8c380a1f970bf37
rejuvyesh <mail@rejuvyesh.com>
2014-06-11 18:12:14 -0400

    catMaybe

--- src/Main.hs
+++ src/Main.hs
@@ -2,27 +2,27 @@
 
 module Main where
 
-import Control.Applicative ((<$>))
-import Control.Applicative.Extras ((<$$>))
-import Data.Either.Extras (bimapEither)
-import Data.List.Extras (substitute)
-import Data.Maybe (fromMaybe, catMaybes)
-import Data.Monoid ((<>), Monoid (mempty, mconcat))
-import Data.Markdown.Extras
-import System.Environment (getArgs)
-import System.Exit (exitSuccess, exitFailure)
-import System.IO (hPutStr, stderr)
-import Text.Kindle.Clippings (Clipping(..), Document(..), Content(..), readClippings)
-import Text.Parsec (parse)
-import Text.Pandoc
-import Text.Pandoc.Builder as TPB
+import Control.Applicative ( (<$>) )
+import Control.Applicative.Extras ( (<$$>) )
+import Data.Either.Extras ( bimapEither )
+import Data.List.Extras ()
+import Data.Maybe ( catMaybes )
+import Data.Monoid ( Monoid(mconcat) )
+import Data.Markdown.Extras ( encodeMarkdown )
+import System.Environment ( getArgs )
+import System.Exit ( exitSuccess, exitFailure )
+import System.IO ( hPutStr, stderr )
+import Text.Kindle.Clippings ( Clipping, readClippings )
+import Text.Parsec ( parse )
+import Text.Pandoc ( def, Pandoc(Pandoc), Block, writeMarkdown )
+import Text.Pandoc.Builder as TPB ( toList )
 
 getClippings :: String -> Either String [Clipping]
 getClippings = bimapEither show catMaybes 
              . parse readClippings [] 
 
 renderClippings :: [Clipping] -> [Block]
-renderClippings = mconcat . fmap TPB.toList . fmap encodeMarkdown
+renderClippings = mconcat . fmap TPB.toList . catMaybes . fmap encodeMarkdown
 
 main :: IO ()
 main = head <$> getArgs >>= getClippings <$$> readFile >>= \case
